f872bcd51440ed816953ba7e0cb8b7666933e149
aj37392580@gmail.com <aj37392580@gmail.com>
2022-11-10 10:33:49 +0800

    增加部分细节

--- src/main/java/com/example/dailyeconomicflow/util/ReturnUtil.java
+++ src/main/java/com/example/dailyeconomicflow/util/ReturnUtil.java
@@ -3,16 +3,21 @@ package com.example.dailyeconomicflow.util;
 import com.example.dailyeconomicflow.pojo.ReturnInfo;
 
 public class ReturnUtil {
-    private static ReturnInfo returnInfo = new ReturnInfo();
+    private static final ReturnInfo returnInfo = new ReturnInfo();
     public static ReturnInfo error(String retMag){
+        //放入 标识 1 代表错误信息
         returnInfo.setRetFlag(1);
+        //放入错误提示信息
         returnInfo.setRetMsg(retMag);
         returnInfo.setRetVal("");
         return returnInfo;
     }
     public static ReturnInfo success(Object retVal){
+        //放入 标识 0 代表成功信息
         returnInfo.setRetFlag(0);
+        // 放入默认成功信息
         returnInfo.setRetMsg("导入成功");
+        //成功之后传递的val值
         returnInfo.setRetVal(retVal);
         return returnInfo;
     }
